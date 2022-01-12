665267d619c6903628a3f398d053a3b542c693ff
Jared Barrus <jeb218@gmail.com>
2022-01-12 14:48:00 -0700

    adding some Instant versions of DateTimes' DateTime utility methods

--- crux-util/src/main/java/com/fizzed/crux/util/DateTimes.java
+++ crux-util/src/main/java/com/fizzed/crux/util/DateTimes.java
@@ -19,6 +19,7 @@ import java.time.Instant;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Duration;
+import static java.util.Objects.isNull;
 
 public class DateTimes {
  
@@ -341,6 +342,287 @@ public class DateTimes {
             return null;
         }
         return JavaTimes.uptime(javaDuration(duration));
+    }
+
+    /**
+     * Returns a if less than or equal to b, otherwise b.
+     * @param a
+     * @param b
+     * @return
+     */
+    static public Instant min(Instant a, Instant b) {
+        if (b == null) {
+            return a;
+        } else if (a == null) {
+            return b;
+        }
+        return a.isAfter(b) ? b : a;
+    }
+
+    /**
+     * Returns the minimum of all datetimes.
+     *
+     * @param dts
+     * @return
+     */
+    static public Instant min(Instant... dts) {
+        if (dts == null) {
+            return null;
+        }
+        Instant v = null;
+        for (Instant dt : dts) {
+            v = min(v, dt);
+        }
+        return v;
+    }
+
+    /**
+     * Returns a if greater than or equal to b, otherwise b.
+     * @param a
+     * @param b
+     * @return
+     */
+    static public Instant max(Instant a, Instant b) {
+        if (b == null) {
+            return a;
+        } else if (a == null) {
+            return b;
+        }
+        return a.isBefore(b) ? b : a;
+    }
+
+    /**
+     * Returns the minimum of all datetimes.
+     *
+     * @param dts
+     * @return
+     */
+    static public Instant max(Instant... dts) {
+        if (dts == null) {
+            return null;
+        }
+        Instant v = null;
+        for (Instant dt : dts) {
+            v = max(v, dt);
+        }
+        return v;
+    }
+
+    /**
+     * Is <code>a</code> value greater than <code>b</code>. If a is not null
+     * and b is null then this is true.
+     * @param a
+     * @param b
+     * @return
+     */
+    static public boolean gt(Instant a, Instant b) {
+        if (a == null) {
+            // if a is null then even if b is null it can't be greater than
+            return false;
+        }
+        if (b == null) {
+            // if a is NOT null and b is then its greater than
+            return true;
+        }
+        return a.isAfter(b);
+    }
+
+    /**
+     * Is <code>a</code> value greater than or equal to <code>b</code>. Will 
+     * still be true if both values are null OR if a is non-null and b is null.
+     * @param a
+     * @param b
+     * @return
+     */
+    static public boolean gte(Instant a, Instant b) {
+        if (a == null) {
+            // if a is null and b is null then this is true
+            return b == null;
+        }
+        if (b == null) {
+            // if a is NOT null and b is then its greater than
+            return true;
+        }
+        return a.compareTo(b) >= 0;
+    }
+
+    /**
+     * Is <code>a</code> value less than <code>b</code>. If b is not null
+     * and a is null then this is true.
+     * @param a
+     * @param b
+     * @return
+     */
+    static public boolean lt(Instant a, Instant b) {
+        if (b == null) {
+            // if a is null then even if b is null it can't be greater than
+            return false;
+        }
+        if (a == null) {
+            // if a is NOT null and b is then its greater than
+            return true;
+        }
+        return a.isBefore(b);
+    }
+
+    /**
+     * Is <code>a</code> value less than or equal to <code>b</code>. Will 
+     * still be true if both values are null OR if a is non-null and b is null.
+     * @param a
+     * @param b
+     * @return
+     */
+    static public boolean lte(Instant a, Instant b) {
+        if (b == null) {
+            // if a is null and b is null then this is true
+            return a == null;
+        }
+        if (a == null) {
+            // if a is NOT null and b is then its greater than
+            return true;
+        }
+        return a.compareTo(b) <= 0;
+    }
+
+    /**
+     * Whether datetime is within start and end datetimes. The min of start and
+     * end is detected so the ordering does not matter.  By default, this method
+     * makes the start date INCLUSIVE and the end date EXCLUSIVE.
+     * @param dt The datetime to check
+     * @param start The start (or end) of the datetime range
+     * @param end The end (or start) of the datetime range
+     * @return True if within range, otherwise false.
+     */
+    static public boolean within(Instant dt, Instant start, Instant end) {
+        return within(dt, start, end, true, false);
+    }
+
+    /**
+     * Whether datetime is within start and end datetimes.The min of start and
+     * end is detected so the ordering does not matter. By default, this method
+     * makes the start date INCLUSIVE and the end date EXCLUSIVE/INCLUSIVE depending
+     * on what you pass in.
+     * @param dt The datetime to check
+     * @param start The start (or end) of the datetime range
+     * @param end The end (or start) of the datetime range
+     * @param endInclusive True if the end date is inclusive, otherwise exclusive.
+     * @return True if within range, otherwise false.
+     */
+    static public boolean within(Instant dt, Instant start, Instant end, boolean endInclusive) {
+        return within(dt, start, end, true, endInclusive);
+    }
+
+    /**
+     * Whether datetime is within start and end datetimes.The min of start and
+     * end is detected so the ordering does not matter.By default, this method
+     * makes the start date INCLUSIVE and the end date EXCLUSIVE/INCLUSIVE depending
+     * on what you pass in.
+     * @param dt The datetime to check
+     * @param start The start (or end) of the datetime range
+     * @param end The end (or start) of the datetime range
+     * @param startInclusive True if the start date is inclusive, otherwise exclusive.
+     * @param endInclusive True if the end date is inclusive, otherwise exclusive.
+     * @return True if within range, otherwise false.
+     */
+    static public boolean within(Instant dt, Instant start, Instant end, boolean startInclusive, boolean endInclusive) {
+        if (dt == null) {
+            return false;
+        }
+
+        // intelligentally pick start/end
+        final Instant _start = min(start, end);
+        final Instant _end = max(start, end);
+
+        if (startInclusive && lt(dt, _start)) {
+            return false;
+        }
+
+        if (!startInclusive && lte(dt, _start)) {
+            return false;
+        }
+
+        if (endInclusive && gt(dt, _end)) {
+            return false;
+        }
+
+        if (!endInclusive && gte(dt, _end)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Is there an overlap in range1 (<code>start1</code> to <code>end1</code>)  and range2 (<code>start2</code> to <code>end2</code>) 
+     * null values are treated as <code>Instant.MIN</code> for start and <code>Instant.MAX</code> for end. By default, this method
+     * makes the start date INCLUSIVE and the end date EXCLUSIVE.
+     * @param start1 - start of range1
+     * @param end1 - end of range1
+     * @param start2 - start of range2
+     * @param end2 - end of range2
+     * @return boolean
+     */
+    static public boolean overlap(Instant start1, Instant end1, Instant start2, Instant end2) {
+        return overlap(start1, end1, start2, end2, true, false);
+    }
+    
+    /**
+     * Is there an overlap in range1 (<code>start1</code> to <code>end1</code>)  and range2 (<code>start2</code> to <code>end2</code>) 
+     * null values are treated as <code>Instant.MIN</code> for start and <code>Instant.MAX</code> for end. By default, this method
+     * makes the start date INCLUSIVE and the end date EXCLUSIVE.
+     * @param start1 - start of range1
+     * @param end1 - end of range1
+     * @param start2 - start of range2
+     * @param end2 - end of range2
+     * @param endInclusive True if the end date is inclusive, otherwise exclusive.
+     * @return boolean
+     */
+    static public boolean overlap(Instant start1, Instant end1, Instant start2, Instant end2, boolean endInclusive) {
+        return overlap(start1, end1, start2, end2, true, endInclusive);
+    }
+
+    /**
+     * Is there an overlap in range1 (<code>start1</code> to <code>end1</code>)  and range2 (<code>start2</code> to <code>end2</code>) 
+     * null values are treated as <code>Instant.MIN</code> for start and <code>Instant.MAX</code> for end. By default, this method
+     * makes the start date INCLUSIVE and the end date EXCLUSIVE.
+     * @param start1 - start of range1
+     * @param end1 - end of range1
+     * @param start2 - start of range2
+     * @param end2 - end of range2
+     * @param startInclusive True if the start date is inclusive, otherwise exclusive.
+     * @param endInclusive True if the end date is inclusive, otherwise exclusive.
+     * @return boolean
+     */
+    static public boolean overlap(Instant start1, Instant end1, Instant start2, Instant end2, boolean startInclusive, boolean endInclusive) {
+        // if both never end, there is an overlap
+        if (end1 == null && end2 == null) {
+            return true;
+        }
+        
+        // if both have no provided start, there is an overlap
+        if (start1 == null && start2 == null) {
+            return true;
+        }
+        
+        Instant range1Start = start1 == null ? Instant.MIN : start1;
+        Instant range1End = end1 == null ? Instant.MAX : end1;
+        Instant range2Start = start2 == null ? Instant.MIN : start2;
+        Instant range2End = end2 == null ? Instant.MAX : end2;
+
+        if (lt(range2Start, range1Start)) {
+            // swap ranges
+            Instant temp1 = range1Start;
+            Instant temp2 = range1End;
+            range1Start = range2Start;
+            range1End = range2End;
+            range2Start = temp1;
+            range2End = temp2;
+        }
+        
+        boolean range1EndsBeforeRange2Starts = endInclusive ? lt(range1End, range2Start) : lte(range1End, range2Start);
+        boolean range1StartsAfterRange2Ends = startInclusive ? gt(range1Start, range2End) : gte(range1Start, range2End);
+        
+        return !(range1EndsBeforeRange2Starts || range1StartsAfterRange2Ends);
     }
     
 }
--- crux-util/src/test/java/com/fizzed/crux/util/DateTimesTest.java
+++ crux-util/src/test/java/com/fizzed/crux/util/DateTimesTest.java
@@ -49,7 +49,9 @@ public class DateTimesTest {
         DateTime d = DateTime.parse("2019-04-04T01:03:03.456Z");
         
         // default joda is null
-        assertThat(DateTimes.min(null, null), is(nullValue()));
+        DateTime n1 = null;
+        DateTime n2 = null;
+        assertThat(DateTimes.min(n1, n2), is(nullValue()));
         assertThat(DateTimes.min(a, null), is(a));
         assertThat(DateTimes.min(null, b), is(b));
         assertThat(DateTimes.min(a, b), is(a));
@@ -68,7 +70,9 @@ public class DateTimesTest {
         DateTime d = DateTime.parse("2019-04-01T01:03:03.456Z");
         
         // default joda is null
-        assertThat(DateTimes.max(null, null), is(nullValue()));
+        DateTime n1 = null;
+        DateTime n2 = null;
+        assertThat(DateTimes.max(n1, n2), is(nullValue()));
         assertThat(DateTimes.max(a, null), is(a));
         assertThat(DateTimes.max(null, b), is(b));
         assertThat(DateTimes.max(a, b), is(a));
@@ -83,8 +87,10 @@ public class DateTimesTest {
     public void gt() {
         DateTime a = DateTime.parse("2019-04-02T01:02:03.456Z");
         DateTime b = DateTime.parse("2019-04-02T01:03:03.456Z");
-        
-        assertThat(DateTimes.gt(null, null), is(false));
+
+        DateTime n1 = null;
+        DateTime n2 = null;
+        assertThat(DateTimes.gt(n1, n2), is(false));
         assertThat(DateTimes.gt(null, a), is(false));
         assertThat(DateTimes.gt(a, b), is(false));
         assertThat(DateTimes.gt(a, a), is(false));
@@ -97,8 +103,10 @@ public class DateTimesTest {
     public void gte() {
         DateTime a = DateTime.parse("2019-04-02T01:02:03.456Z");
         DateTime b = DateTime.parse("2019-04-02T01:03:03.456Z");
-        
-        assertThat(DateTimes.gte(null, null), is(true));
+
+        DateTime n1 = null;
+        DateTime n2 = null;
+        assertThat(DateTimes.gte(n1, n2), is(true));
         assertThat(DateTimes.gte(null, a), is(false));
         assertThat(DateTimes.gte(a, b), is(false));
         assertThat(DateTimes.gte(a, a), is(true));
@@ -111,8 +119,10 @@ public class DateTimesTest {
     public void lt() {
         DateTime b = DateTime.parse("2019-04-02T01:02:03.456Z");
         DateTime a = DateTime.parse("2019-04-02T01:03:03.456Z");
-        
-        assertThat(DateTimes.lt(null, null), is(false));
+
+        DateTime n1 = null;
+        DateTime n2 = null;
+        assertThat(DateTimes.lt(n1, n2), is(false));
         assertThat(DateTimes.lt(null, a), is(true));
         assertThat(DateTimes.lt(a, b), is(false));
         assertThat(DateTimes.lt(a, a), is(false));
@@ -126,8 +136,10 @@ public class DateTimesTest {
     public void lte() {
         DateTime b = DateTime.parse("2019-04-02T01:02:03.456Z");
         DateTime a = DateTime.parse("2019-04-02T01:03:03.456Z");
-        
-        assertThat(DateTimes.lte(null, null), is(true));
+
+        DateTime n1 = null;
+        DateTime n2 = null;
+        assertThat(DateTimes.lte(n1, n2), is(true));
         assertThat(DateTimes.lte(null, a), is(true));
         assertThat(DateTimes.lte(a, b), is(false));
         assertThat(DateTimes.lte(a, a), is(true));
@@ -197,5 +209,221 @@ public class DateTimesTest {
         assertThat(DateTimes.uptime(dt7, dt0), is("9d 1h 1s"));
         assertThat(DateTimes.uptime(dt8, dt0), is("9d 2h 8m 59s 356ms"));
     }
+
+    @Test
+    public void minInstant() {
+        Instant a = Instant.parse("2019-04-02T01:02:03.456Z");
+        Instant b = Instant.parse("2019-04-02T01:03:03.456Z");
+        Instant c = Instant.parse("2019-04-01T01:03:03.456Z");
+        Instant d = Instant.parse("2019-04-04T01:03:03.456Z");
+
+        // default joda is null
+        Instant n1 = null;
+        Instant n2 = null;
+        assertThat(DateTimes.min(n1, n2), is(nullValue()));
+        assertThat(DateTimes.min(a, null), is(a));
+        assertThat(DateTimes.min(null, b), is(b));
+        assertThat(DateTimes.min(a, b), is(a));
+        assertThat(DateTimes.min(b, a), is(a));
+
+        assertThat(DateTimes.min(a, b, c, d), is(c));
+        assertThat(DateTimes.min(null, a, b, c, d), is(c));
+        assertThat(DateTimes.min((Instant[])null), is(nullValue()));
+    }
+
+    @Test
+    public void maxInstant() {
+        Instant b = Instant.parse("2019-04-02T01:02:03.456Z");
+        Instant a = Instant.parse("2019-04-02T01:03:03.456Z");
+        Instant c = Instant.parse("2019-04-03T01:03:03.456Z");
+        Instant d = Instant.parse("2019-04-01T01:03:03.456Z");
+
+        // default joda is null
+        Instant n1 = null;
+        Instant n2 = null;
+        assertThat(DateTimes.max(n1, n2), is(nullValue()));
+        assertThat(DateTimes.max(a, null), is(a));
+        assertThat(DateTimes.max(null, b), is(b));
+        assertThat(DateTimes.max(a, b), is(a));
+        assertThat(DateTimes.max(b, a), is(a));
+
+        assertThat(DateTimes.max(a, b, c, d), is(c));
+        assertThat(DateTimes.max(null, b, c, d), is(c));
+        assertThat(DateTimes.max((Instant[])null), is(nullValue()));
+    }
+
+    @Test
+    public void gtInstant() {
+        Instant a = Instant.parse("2019-04-02T01:02:03.456Z");
+        Instant b = Instant.parse("2019-04-02T01:03:03.456Z");
+
+        Instant n1 = null;
+        Instant n2 = null;
+        assertThat(DateTimes.gt(n1, n2), is(false));
+        assertThat(DateTimes.gt(null, a), is(false));
+        assertThat(DateTimes.gt(a, b), is(false));
+        assertThat(DateTimes.gt(a, a), is(false));
+        assertThat(DateTimes.gt(b, b), is(false));
+        assertThat(DateTimes.gt(b, a), is(true));
+        assertThat(DateTimes.gt(a, null), is(true));
+    }
+
+    @Test
+    public void gteInstant() {
+        Instant a = Instant.parse("2019-04-02T01:02:03.456Z");
+        Instant b = Instant.parse("2019-04-02T01:03:03.456Z");
+
+        Instant n1 = null;
+        Instant n2 = null;
+        assertThat(DateTimes.gte(n1, n2), is(true));
+        assertThat(DateTimes.gte(null, a), is(false));
+        assertThat(DateTimes.gte(a, b), is(false));
+        assertThat(DateTimes.gte(a, a), is(true));
+        assertThat(DateTimes.gte(b, b), is(true));
+        assertThat(DateTimes.gte(b, a), is(true));
+        assertThat(DateTimes.gte(a, null), is(true));
+    }
+
+    @Test
+    public void ltInstant() {
+        Instant b = Instant.parse("2019-04-02T01:02:03.456Z");
+        Instant a = Instant.parse("2019-04-02T01:03:03.456Z");
+
+        Instant n1 = null;
+        Instant n2 = null;
+        assertThat(DateTimes.lt(n1, n2), is(false));
+        assertThat(DateTimes.lt(null, a), is(true));
+        assertThat(DateTimes.lt(a, b), is(false));
+        assertThat(DateTimes.lt(a, a), is(false));
+        assertThat(DateTimes.lt(b, b), is(false));
+        assertThat(DateTimes.lt(b, a), is(true));
+        assertThat(DateTimes.lt(a, null), is(false));
+    }
+
+    @Test
+    public void lteInstant() {
+        Instant b = Instant.parse("2019-04-02T01:02:03.456Z");
+        Instant a = Instant.parse("2019-04-02T01:03:03.456Z");
+
+        Instant n1 = null;
+        Instant n2 = null;
+        assertThat(DateTimes.lte(n1, n2), is(true));
+        assertThat(DateTimes.lte(null, a), is(true));
+        assertThat(DateTimes.lte(a, b), is(false));
+        assertThat(DateTimes.lte(a, a), is(true));
+        assertThat(DateTimes.lte(b, b), is(true));
+        assertThat(DateTimes.lte(b, a), is(true));
+        assertThat(DateTimes.lte(a, null), is(false));
+    }
+
+    @Test
+    public void withinInstant() {
+
+        Instant dt0 = Instant.parse("2019-04-01T01:03:03.456Z");
+        Instant dt1 = Instant.parse("2019-04-02T01:03:03.456Z");
+        Instant dt2 = Instant.parse("2019-04-03T01:03:03.456Z");
+        Instant dt3 = Instant.parse("2019-04-04T01:03:03.456Z");
+        Instant dt4 = Instant.parse("2019-04-05T01:03:03.456Z");
+
+        assertThat(DateTimes.within(null, dt0, dt2, true, true), is(false));
+        assertThat(DateTimes.within(dt1, null, null, true, true), is(false));
+        assertThat(DateTimes.within(dt1, null, dt2, true, true), is(false));
+        assertThat(DateTimes.within(dt1, dt2, null, true, true), is(false));
+        assertThat(DateTimes.within(dt1, dt0, dt2, true, true), is(true));
+        assertThat(DateTimes.within(dt1, dt2, dt0, true, true), is(true));      // auto picked?
+        assertThat(DateTimes.within(dt1, dt0, dt2, false, false), is(true));
+        assertThat(DateTimes.within(dt0, dt2, dt3, true, true), is(false));
+        assertThat(DateTimes.within(dt0, dt3, dt4, true, true), is(false));
+        assertThat(DateTimes.within(dt0, dt0, dt0, true, true), is(true));
+        assertThat(DateTimes.within(dt0, dt0, dt0, false, true), is(false));
+        assertThat(DateTimes.within(dt0, dt0, dt0, true, false), is(false));
+        assertThat(DateTimes.within(dt0, dt0, dt0, true, false), is(false));
+        assertThat(DateTimes.within(dt3, dt0, dt4, true, true), is(true));
+        assertThat(DateTimes.within(dt4, dt0, dt4, true, true), is(true));
+        assertThat(DateTimes.within(dt4, dt0, dt4, true, false), is(false));
+        assertThat(DateTimes.within(dt0, dt0, dt4, true, true), is(true));
+        assertThat(DateTimes.within(dt0, dt0, dt4, false, true), is(false));
+        assertThat(DateTimes.within(dt0, dt4, dt0, false, true), is(false));
+    }
     
+    @Test
+    public void overlapInstant() {
+        // range 2 starts at the exact same instant that range1 ends
+        Instant start1 = Instant.parse("2022-01-01T00:00:00.000000Z");
+        Instant end1 = Instant.parse("2022-01-02T00:00:00.000000Z");
+        Instant start2 = Instant.parse("2022-01-02T00:00:00.000000Z");
+        Instant end2 = Instant.parse("2022-01-03T00:00:00.000000Z");
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, true, true), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, true), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, false), is(false));
+
+        // force ranges to be swapped so the order doesn't matter
+        assertThat(DateTimes.overlap(start2, end2, start1, end1), is(false));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, true, true), is(true));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, false, true), is(true));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, false, false), is(false));
+
+        // all nulls.... always overlaps...
+        assertThat(DateTimes.overlap(null, null, null, null), is(true));
+        assertThat(DateTimes.overlap(null, null, null, null, true, true), is(true));
+        assertThat(DateTimes.overlap(null, null, null, null, false, true), is(true));
+        assertThat(DateTimes.overlap(null, null, null, null, false, false), is(true));
+
+        // range 2 starts .000001 after range 1 ends
+        start1 = Instant.parse("2022-01-01T00:00:00.000000Z");
+        end1 = Instant.parse("2022-01-02T00:00:00.000000Z");
+        start2 = Instant.parse("2022-01-02T00:00:00.000001Z");
+        end2 = Instant.parse("2022-01-03T00:00:00.000000Z");
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, true, true), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, true), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, false), is(false));
+
+        // force ranges to be swapped so the order doesn't matter
+        assertThat(DateTimes.overlap(start2, end2, start1, end1), is(false));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, true, true), is(false));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, false, true), is(false));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, false, false), is(false));
+
+        // range 2 had no start and range1 is before range2 end
+        start1 = Instant.parse("2022-01-01T00:00:00.000000Z");
+        end1 = Instant.parse("2022-01-02T00:00:00.000000Z");
+        start2 = null;
+        end2 = Instant.parse("2022-01-03T00:00:00.000000Z");
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, true, true), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, true), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, false), is(true));
+
+        // range2 completely within range1 
+        start1 = Instant.parse("2022-01-01T00:00:00.000000Z");
+        end1 = Instant.parse("2022-01-10T00:00:00.000000Z");
+        start2 = Instant.parse("2022-01-02T00:00:00.000001Z");
+        end2 = Instant.parse("2022-01-03T00:00:00.000000Z");
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, true, true), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, true), is(true));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, false), is(true));
+
+        // force ranges to be swapped so the order doesn't matter
+        assertThat(DateTimes.overlap(start2, end2, start1, end1), is(true));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, true, true), is(true));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, false, true), is(true));
+        assertThat(DateTimes.overlap(start2, end2, start1, end1, false, false), is(true));
+
+        // range 2 had no end and range1 is before range2 start
+        start1 = Instant.parse("2022-01-01T00:00:00.000000Z");
+        end1 = Instant.parse("2022-01-02T00:00:00.000000Z");
+        start2 = Instant.parse("2022-01-03T00:00:00.000000Z");
+        end2 = null;
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, true, true), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, true), is(false));
+        assertThat(DateTimes.overlap(start1, end1, start2, end2, false, false), is(false));
+    }
 }
