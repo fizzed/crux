9cffb2a96dbbf750ae46973aed5ecd7671e2b412
Joe Lauer <joe@lauer.bz>
2025-01-20 13:24:29 -0500

    Blaze build uses public project v1.0.0

--- .blaze/blaze.java
+++ .blaze/blaze.java
@@ -1,35 +1,7 @@
-import com.fizzed.blaze.Task;
 import com.fizzed.blaze.project.PublicBlaze;
-import com.fizzed.buildx.Buildx;
-import com.fizzed.buildx.Target;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 public class blaze extends PublicBlaze {
 
-    private final List<Target> crossTestTargets = asList(
-        new Target("linux", "x64").setTags("test").setHost("bmh-build-x64-linux-latest"),
-        new Target("linux", "arm64").setTags("test").setHost("bmh-build-arm64-linux-latest"),
-        new Target("linux", "riscv64").setTags("test").setHost("bmh-build-riscv64-linux-latest"),
-        new Target("linux_musl", "x64").setTags("test").setHost("bmh-build-x64-linux-musl-latest"),
-        new Target("macos", "x64").setTags("test").setHost("bmh-build-x64-macos-latest"),
-        new Target("macos", "arm64").setTags("test").setHost("bmh-build-arm64-macos-latest"),
-        new Target("windows", "x64").setTags("test").setHost("bmh-build-x64-windows-latest"),
-        new Target("windows", "arm64").setTags("test").setHost("bmh-build-arm64-windows-latest"),
-        new Target("freebsd", "x64").setTags("test").setHost("bmh-build-x64-freebsd-latest"),
-        new Target("openbsd", "x64").setTags("test").setHost("bmh-build-x64-openbsd-latest")
-    );
-
-    @Task(order = 1)
-    public void cross_tests() throws Exception {
-        new Buildx(crossTestTargets)
-            .tags("test")
-            .execute((target, project) -> {
-                project.action("mvn", "clean", "test")
-                    .run();
-            });
-    }
+    // all public of PublicBlaze included
 
 }
